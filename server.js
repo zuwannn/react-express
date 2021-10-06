b2fd87dccddc00819eeacf73a06563eb4b96dfad
zuwannn <suwanneethongtha@gmail.com>
2021-10-06 18:00:57 +0700

    Add Middleware

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const express = require('express');
 const app = express()
 const books = require('./db.json')
 
+// Middleware
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
 app.get('/', function(req, res){
     res.send('Hello World')
