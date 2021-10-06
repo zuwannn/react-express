76352462e4526786bd935b1d6454e4859437303e
zuwannn <suwanneethongtha@gmail.com>
2021-10-06 17:51:47 +0700

    get  API

--- server.js
+++ server.js
@@ -1,9 +1,22 @@
 const express = require('express');
 const app = express()
+const books = require('./db.json')
 
-app.get('/', (req, res)=>{
+
+app.get('/', function(req, res){
     res.send('Hello World')
 })
+
+// get book by id
+app.get('/books/:id', (req, res)=>{
+    res.json(books.find(book => book.id === req.params.id))
+})
+
+// get all books
+app.get('/books', (req, res)=>{
+    res.json(books)
+})
+
 
 app.listen(3000, () => {
     console.log('start server at port 3000');
