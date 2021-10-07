534ece4703d9955789b09da36c2e139b7fa479a7
zuwannn <suwanneethongtha@gmail.com>
2021-10-07 16:51:02 +0700

    put API
    update book by id

--- server.js
+++ server.js
@@ -20,11 +20,17 @@ app.get('/books', (req, res)=>{
     res.json(books)
 })
 
+// create book
 app.post('/books', (req, res)=>{
     books.push(req.body)
     res.status(201).json(req.body)
 })
 
+// update book by id
+app.put('/books/:id', (req, res)=>{
+    const updateIndex = books.findIndex(book => book.id === req.params.id)
+    res.json(Object.assign(books[updateIndex], req.body))
+})
 app.listen(3000, () => {
     console.log('start server at port 3000');
 })
